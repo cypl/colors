b414e9347efe99affbb2dc4d99a57a512d9b79fb
cypl <cyrille.plenchette@me.com>
2023-07-30 22:57:12 +0200

    add color palette for UI

---
+++ src/utils/colorsUI.js
@@ -0,0 +1,12 @@
+export const colorsUI = {
+    dark: "#1e1e1e",
+    darkest: "rgba(0,0,0,0.25)",
+    darkless: "rgba(120,120,120,0.3)",
+    darkColoredValue: "#e2bd7d",
+    darkText: "#ffffff",
+    light: "#ededed",
+    lightest: "rgba(255,255,255,0.25)",
+    lightless: "rgba(255,255,255,0.3)",
+    lightColoredValue: "#e2bd7d",
+    lightText: "#333333",
+}
