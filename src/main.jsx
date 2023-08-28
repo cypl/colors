88d4b2b13bb8cda2651136edfe39267dafa0aa36
cypl <cyrille.plenchette@me.com>
2023-08-28 22:00:47 +0200

    add modal provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { store } from './store/store'
 import { Provider } from 'react-redux'
+import { ModalProvider } from "react-modal-classic"
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ModalProvider>
+        <App />
+      </ModalProvider>
     </Provider>
   </React.StrictMode>,
 )
