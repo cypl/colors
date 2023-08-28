36add7894035b0d0a0376b65c10b6be7dbc51b28
cypl <cyrille.plenchette@me.com>
2023-08-28 22:00:28 +0200

    add modal themes

---
+++ src/utils/modalThemes.js
@@ -0,0 +1,2 @@
+export const modalThemeAbout = {closeButton: "out", closeButtonColor: "rgba(255,255,255,0.5)", backgroundColor: "transparent", size: "lg", radius:"3px" }
+export const modalThemeColorPicker = {closeButton: "out", closeButtonColor: "rgba(255,255,255,0.5)", backgroundColor: "transparent", size: "md", radius:"3px" }
