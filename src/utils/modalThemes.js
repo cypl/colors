f2ade1e9907b1d79963fcecaeb32311b871fbda2
cypl <cyrille.plenchette@me.com>
2023-12-29 23:45:10 +0100

    add modalThemeExport

--- src/utils/modalThemes.js
+++ src/utils/modalThemes.js
@@ -1,2 +1,5 @@
-export const modalThemeAbout = {closeButton: "out", closeButtonColor: "rgba(255,255,255,0.5)", backgroundColor: "transparent", size: "lg", radius:"3px" }
-export const modalThemeColorPicker = {closeButton: "out", closeButtonColor: "rgba(255,255,255,0.5)", backgroundColor: "transparent", size: "md", radius:"3px" }
+import { sizesUI } from "./UI"
+
+export const modalThemeExport = {closeButton: "out", closeButtonColor: "rgba(255,255,255,0.5)", backgroundColor: "transparent", size: "lg", radius:sizesUI.radius }
+export const modalThemeAbout = {closeButton: "out", closeButtonColor: "rgba(255,255,255,0.5)", backgroundColor: "transparent", size: "md", radius:sizesUI.radius }
+export const modalThemeColorPicker = {closeButton: "out", closeButtonColor: "rgba(255,255,255,0.5)", backgroundColor: "transparent", size: "md", radius:sizesUI.radius }
