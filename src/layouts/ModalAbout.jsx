7d6d551a96de6f82aa4c81a467550aaca66cfd93
cypl <cyrille.plenchette@me.com>
2024-01-09 09:26:30 +0100

    wording

--- src/layouts/ModalAbout.jsx
+++ src/layouts/ModalAbout.jsx
@@ -19,7 +19,7 @@ const ModalContent = styled.div`
 
 const content = {
     text: 
-        ["Nanosite ColorPickers permet de générer une gamme de couleur à partir d'une couleur de départ. Cette gamme peut ensuite être utilisée pour créer un webdesign, ou bien être exportée sous la forme d'un objet javascript ou d'une liste de variables SASS pour être utilisée directement par un développeur.",
+        ["NanoColors permet de générer une gamme de couleur à partir d'une couleur de départ. Cette gamme peut ensuite être utilisée pour créer le webdesign d'un Nanosite, ou bien être exportée sous la forme d'un objet javascript ou d'une liste de variables SASS pour être utilisée directement par un développeur.",
         "Les couleurs sont générées en vérifiant leurs niveaux de luminosité, ce qui permet de s'assurer du contraste qu'elles ont les unes par rapport aux autres et valider certains critères d'accessibilité de l'interface que vous créez."]
 }
 
